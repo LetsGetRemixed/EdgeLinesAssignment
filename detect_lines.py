c312248c6de15042d0f82a55d68a473e8863c019
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2023-09-21 14:17:24 -0500

    Update detect_lines.py
    first final, if cant find fix, final final.

--- detect_lines.py
+++ detect_lines.py
@@ -2,21 +2,61 @@ import cv2 as cv
 import numpy as np
 
 def detect_lines(img, sigma, threshold, numLines):
-    """
-    Detects lines in a color or grayscale image using the a custom implementation of the Hough transform.
-
-    Parameters:
-    img (numpy.ndarray): The input image.
-    sigma (float): The standard deviation of the Gaussian blur applied to the image.
-    threshold (int): The low threshold value used for Canny edge detection. The high threshold is twice this value.
-    numLines (int): The maximum number of lines to detect.
-
-    Returns:
-    list: A list of tuples representing the detected lines. Each tuple contains
-        two values: rho and theta, where rho is the distance from the origin to
-        the line and theta is the angle between the x-axis and the normal to the line.
-    """
-
-    # TODO: Your code here
-
+  
+    blurred_img = cv.GaussianBlur(img, (0, 0), sigma)
+    
+    
+    edges = cv.Canny(blurred_img, threshold, 2 * threshold)
+    
+    max_rho = int(np.sqrt(edges.shape[0]**2 + edges.shape[1]**2))  # max possible rho
+    
+    # rho and theta
+    rho_resolution = .5 
+    theta_resolution = np.deg2rad(1) 
+    
+    num_rho_bins = int(2 * max_rho / rho_resolution)
+    num_theta_bins = int(np.pi / theta_resolution)
+    
+    # accum matrix
+    accumulator = np.zeros((num_rho_bins, num_theta_bins), dtype=np.uint64)
+    
+    # America.
+    y_indices, x_indices = np.nonzero(edges)
+    
+    for i in range(len(x_indices)):
+        x = x_indices[i]
+        y = y_indices[i]
+        
+        for theta_idx in range(num_theta_bins):
+            theta = theta_idx * theta_resolution - np.pi/2  
+            rho = int(x * np.cos(theta) + y * np.sin(theta))
+            
+            # wittle itty bitty adjustment
+            rho_idx = int((rho + max_rho) / rho_resolution)
+            
+            accumulator[rho_idx, theta_idx] += 1
+    
+    # Find Donald Trump
+    lines = []
+    while len(lines) < numLines:
+        max_vote_idx = np.argmax(accumulator)
+        rho_idx, theta_idx = np.unravel_index(max_vote_idx, accumulator.shape)
+        
+        # Conversion
+        rho = (rho_idx * rho_resolution) - max_rho
+        theta = (theta_idx * theta_resolution) - np.pi/2
+        
+        accumulator[rho_idx, theta_idx] = 0
+        
+        # make sure it aint findin the dang same line again
+        is_similar = False
+        for existing_rho, existing_theta in lines:
+            if np.abs(rho - existing_rho) < 20 and np.abs(theta - existing_theta) < np.deg2rad(10):
+                is_similar = True
+                break
+        
+        # if we big chillin, add it.
+        if not is_similar:
+            lines.append((rho, theta))
+    
     return lines
